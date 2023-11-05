cdd2ffea696ea0461511f281f3cb136a7a70141b
pleewson <adrianplewait@gmail.com>
2023-11-05 13:31:46 +0100

    task accepted

---
+++ src/main/java/LeetCode_tasks_1/Palindrom_Number.java
@@ -0,0 +1,43 @@
+package LeetCode_tasks_1;
+
+https://leetcode.com/problems/palindrome-number/
+
+import java.util.ArrayList;
+
+class Palindrom_Number {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121));
+
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int originalX = x;
+
+        while (x > 0) {
+            arrayList.add(x % 10);
+            x = x / 10;
+        }
+
+        int powerOfTen = 1;
+        int reverse = 0;
+
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            reverse += arrayList.get(i) * powerOfTen;
+            powerOfTen *= 10;
+        }
+
+        if (reverse == originalX) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
