81d809b298b948d0d6996ac64747cbddd330857d
pleewson <adrianplewait@gmail.com>
2023-11-27 17:11:01 +0100

    Add_Digits complete - accepted

--- src/main/java/LeetCode_Easy/test.java
+++ src/main/java/LeetCode_Easy/test.java
@@ -3,20 +3,28 @@ package LeetCode_Easy;
 import java.util.Stack;
 
 public class test {
+
     public static void main(String[] args) {
 
-        Stack<String> videoGames = new Stack<>();
 
-        videoGames.add("Super mario bros");
-        videoGames.add("Smash bros ultimate");
-        videoGames.add("Luigi Mansions 3");
-        videoGames.add("God of war");
+        int a = 124;
+        int sum = 0;
 
+       for(int i = 0; i < 3; i++){
+           sum += a % 10;
+           a /= 10;
+       }
 
-
-
-        System.out.println(videoGames.search("Super mario bros"));
-        System.out.println("stack.pop() -> " + videoGames.pop());
-        System.out.println(videoGames);
+        System.out.println(sum);
     }
+
+
+//    //metody klasy test
+//    public void instantMethod(){
+//        System.out.println("instancyjna metoda zostala wywolana");
+//    }
+//
+//    public static void staticMethod(){
+//        System.out.println("statyczna metoda zostala wywolana");
+//    }
 }
