04960db6b0c32437cfd0730e6075c99c0daae333
pleewson <adrianplewait@gmail.com>
2023-12-05 23:57:19 +0100

    new alg close to finish

---
+++ src/main/java/LeetCode_Easy/Remove_duplicates_from_sorted_array.java
@@ -0,0 +1,42 @@
+package LeetCode_Easy;
+
+import java.util.*;
+
+public class Remove_duplicates_from_sorted_array {
+    public static void main(String[] args) {
+
+        int[] array = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        System.out.println(removeDuplicates(array));
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        Iterator<Integer> iterator = set.iterator();
+        int[] noDuplicatesArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+          System.out.println(set +" -> set przed for");
+        for (int i = 0; i < set.size(); i++) {
+            if (iterator.hasNext()) {
+                noDuplicatesArray[i] = iterator.next();
+            }
+        }
+
+        System.out.println("array -> " + Arrays.toString(noDuplicatesArray));
+//        for(int i = set.size(); i < nums.length; i++){
+//            nums[i] = null;
+//        }
+
+        System.out.println(Arrays.toString(set.toArray()));
+
+        int numberOfDuplicates = nums.length - set.size();
+
+        return numberOfDuplicates;
+    }
+}
+
