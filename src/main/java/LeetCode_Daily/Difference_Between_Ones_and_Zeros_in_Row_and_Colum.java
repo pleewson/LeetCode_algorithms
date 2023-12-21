b42b1f4a0191445f23f8080b47e86f22d1527919
pleewson <adrianplewait@gmail.com>
2023-12-21 19:08:03 +0100

    working on it -save 19:07 :

--- src/main/java/LeetCode_Daily/Difference_Between_Ones_and_Zeros_in_Row_and_Colum.java
+++ src/main/java/LeetCode_Daily/Difference_Between_Ones_and_Zeros_in_Row_and_Colum.java
@@ -5,9 +5,9 @@ public class Difference_Between_Ones_and_Zeros_in_Row_and_Colum {
     public static void main(String[] args) {
 
         int[][] arr = {
-                {0, 1, 1},
-                {1, 0, 1},
-                {0, 0, 1}
+                {1,1,1},
+                {1,1,1}
+
         };
 
 
@@ -16,7 +16,7 @@ public class Difference_Between_Ones_and_Zeros_in_Row_and_Colum {
         for (int i = 0; i < arrrrr.length; i++) {
             System.out.println();
             for (int j = 0; j < arrrrr[i].length; j++) {
-                System.out.println(arrrrr[i][j]);
+                System.out.print(arrrrr[i][j]);
             }
         }
     }
@@ -26,39 +26,39 @@ public class Difference_Between_Ones_and_Zeros_in_Row_and_Colum {
 
         int[][] outputArr = new int[grid.length][grid.length];
 
-        int x = 0;
-
         for (int i = 0; i < grid.length; i++) {
+            int columnIndex = 0;
 
-            int onesRow = 0;
-            int zerosRow = 0;
-            int onesCol = 0;
-            int zeroesCol = 0;
-            int diff = 0;
+            while (columnIndex < grid.length) {
 
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 0) {
-                    zerosRow++;
-                } else if (grid[i][j] == 1) {
-                    onesRow++;
+                int onesRow = 0;
+                int zerosRow = 0;
+                int onesCol = 0;
+                int zeroesCol = 0;
+                int diff;
+
+                //rows
+                for (int j = 0; j < grid[i].length; j++) {
+                    if (grid[i][j] == 0) {
+                        zerosRow++;
+                    } else if (grid[i][j] == 1) {
+                        onesRow++;
+                    }
+
+                    //columns
+                    
+                    if (grid[j][columnIndex] == 0) {
+                        zeroesCol++;
+                    } else if (grid[j][columnIndex] == 1) {
+                        onesCol++;
+                    }
                 }
 
-                if (grid[j][i] == 0) {
-                    zeroesCol++;
-                } else if (grid[j][i] == 1) {
-                    onesCol++;
-                }
+                diff = onesRow + onesCol - zerosRow - zeroesCol;
+
+                outputArr[i][columnIndex] = diff;
+                columnIndex++;
             }
-
-            diff = onesRow + onesCol - zerosRow - zeroesCol;
-
-            if (x > grid.length) {
-                x = 0;
-            }
-
-            outputArr[i][x] = diff;
-
-            x++;
         }
 
         return outputArr;
