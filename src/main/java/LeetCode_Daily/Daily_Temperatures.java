b91e0769be2d9aac97d46a3a3620dcca3a458c23
pleewson <adrianplewait@gmail.com>
2024-02-01 00:16:49 +0100

    daily temperatures -deque method

--- src/main/java/LeetCode_Daily/Daily_Temperatures.java
+++ src/main/java/LeetCode_Daily/Daily_Temperatures.java
@@ -5,12 +5,37 @@ import java.util.*;
 public class Daily_Temperatures {
     public static void main(String[] args) {
 
-        int[] temperatures = {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,96,99,99,99,99,0};
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
         System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
-//queue
-        //73 74 75 71 69 72 76 73
+
     }
 
+    public static int[] dailyTemperatures(int[] temepratures) {
+        
+        int[] outputArr = new int[temepratures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = temepratures.length - 1; i >= 0; i--) {
+            int currentTemperature = temepratures[i];
+
+            while (!stack.isEmpty() && currentTemperature >= temepratures[stack.peekLast()]) {
+                stack.pollLast();
+            }
+
+            if (!stack.isEmpty()) {
+                outputArr[i] = stack.peekLast() - i;
+            }
+
+            stack.offerLast(i);
+
+        }
+
+        return outputArr;
+    }
+
+
+    //last test not accepted
+    /*
     public static int[] dailyTemperatures(int[] temperatures) {
         Deque<Integer> deque = new ArrayDeque<>();
 
@@ -37,4 +62,6 @@ public class Daily_Temperatures {
 
         return res;
     }
+     */
+
 }
