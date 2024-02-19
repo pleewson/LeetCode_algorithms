f1e46dfab65433e8a2983bbe0432e91372648ed5
pleewson <adrianplewait@gmail.com>
2024-02-19 23:56:21 +0100

    Majority_element

---
+++ src/main/java/LeetCode_Easy/Majority_Element.java
@@ -0,0 +1,12 @@
+package LeetCode_Easy;
+
+public class Majority_Element {
+    public static void main(String[] args) {
+
+    }
+
+    public int majorityElement(int[] nums) {
+
+        return -1;
+    }
+}
