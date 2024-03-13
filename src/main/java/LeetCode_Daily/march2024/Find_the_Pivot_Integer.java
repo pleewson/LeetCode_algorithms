1fb56fdc3b049b2b5d8c7510f2b0b3e76a570476
pleewson <adrianplewait@gmail.com>
2024-03-13 10:38:15 +0100

    Find_the_Pivot_Integer -daily -accepted

---
+++ src/main/java/LeetCode_Daily/march2024/Find_the_Pivot_Integer.java
@@ -0,0 +1,30 @@
+package LeetCode_Daily.march2024;
+// https://leetcode.com/problems/find-the-pivot-integer/submissions/1202393065/?envType=daily-question&envId=2024-03-13
+
+public class Find_the_Pivot_Integer {
+    public static void main(String[] args) {
+        System.out.println(pivotInteger(8));
+    }
+
+    public static int pivotInteger(int n) {
+
+        int[] sumArray = new int[n];
+        int prefixSum = 0;
+        for (int i = 1; i <= n; i++) {
+            prefixSum += i;
+            sumArray[i-1] = prefixSum;
+        }
+
+        int oppositePrefixSum = 0;
+        for(int i = n; i > 0; i--){
+            oppositePrefixSum += i;
+
+            if(oppositePrefixSum == sumArray[i-1]){
+                return i;
+            }
+        }
+
+
+        return -1;
+    }
+}
