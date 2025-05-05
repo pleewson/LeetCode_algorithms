edf03fc986096c92220e50079cbc4d859f886ba5
pleewson <adrianplewait@gmail.com>
2025-05-05 22:45:58 +0200

    Spiral_Matrix_II -almost finished

--- src/main/java/LeetCode_Medium/Spiral_Matrix_II.java
+++ src/main/java/LeetCode_Medium/Spiral_Matrix_II.java
@@ -4,7 +4,7 @@ package LeetCode_Medium;
 
 public class Spiral_Matrix_II {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
 
         System.out.println(generateMatrix(n));
 
@@ -14,38 +14,79 @@ public class Spiral_Matrix_II {
         int num = 1;
         int[][] outputArr = new int[n][n];
 
-//        int h = 0;
-//        while ()
+        int index = 0;
+        while (num != ((n*n)+1)) {
 
-            for (int i = 0; i < n; i++) {
-                outputArr[0][i] = num;
+            //to right
+            System.out.println("to right");
+            for (int i = index; i < (n - index); i++) {
+                outputArr[0+index][i] = num;
+                System.out.print(outputArr[0+index][i]);
                 num++;
+                if((num == ((n*n)+1))){
+                    break;
+                }
             }
 
-        for (int i = 1; i < n; i++) {
-            outputArr[i][n - 1] = num;
-            num++;
+            System.out.println();
+
+            //down
+            System.out.println("down");
+            for (int i = (index + 1); i < (n - index - 1); i++) {
+//                System.out.println("I = " + i);
+//                System.out.println("n = " + n);
+//                System.out.println("Index = " + index);
+//                System.out.println("rownianie = " + (n - index - 1));
+                outputArr[i][n - index - 1] = num;
+                System.out.print(outputArr[i][n - index - 1]);
+                num++;
+                if((num == ((n*n)+1))){
+                    break;
+                }
+            }
+
+            System.out.println();
+
+
+            //to left
+            System.out.println("to left");
+            for (int i = (n - index - 1); i >= index; i--) {
+                outputArr[n - index - 1][i] = num;
+                System.out.print(outputArr[n - index - 1][i]);
+                num++;
+                if((num == ((n*n)+1))){
+                    break;
+                }
+            }
+
+            System.out.println();
+
+            //up
+            System.out.println("up");
+            for (int i = (n - index - 2); i >= (index + 1); i--) {
+                outputArr[i][index] = num;
+                System.out.print(outputArr[i][index]);
+                num++;
+                if((num == ((n*n)+1))){
+                    break;
+                }
+            }
+
+            System.out.println();
+            index++;
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            outputArr[n - 1][i] = num;
-            num++;
-        }
 
-        for (int i = n - 2; i >= 1; i--) {
-            outputArr[i][0] = num;
-            num++;
-
-        }
-
+        //print
         for (int i = 0; i < outputArr.length; i++) {
             System.out.println();
             for (int j = 0; j < outputArr[i].length; j++) {
-                System.out.print(outputArr[i][j]);
+                System.out.print(outputArr[i][j] + ", ");
             }
         }
 
         return null;
     }
+
 
 }
