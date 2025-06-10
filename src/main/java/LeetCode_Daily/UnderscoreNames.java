c369a5fee5770dc9c65e5fc44fb51c20734ecd8b
pleewson <adrianplewait@gmail.com>
2025-06-10 13:21:06 +0200

    Maximum_Difference_Between_Even_and_Odd_Frequency_I -daily -accepted

--- src/main/java/LeetCode_Daily/UnderscoreNames.java
+++ src/main/java/LeetCode_Daily/UnderscoreNames.java
@@ -2,7 +2,7 @@ package LeetCode_Daily;
 
 public class UnderscoreNames {
     public static void main(String[] args) {
-        String name = "Divisible and Non-divisible Sums Difference";
+        String name = "Maximum Difference Between Even and Odd Frequency I";
         addUnderscores(name);
     }
 
