7ffb1791e305815c284814433d277cdb265c867d
pleewson <adrianplewait@gmail.com>
2025-06-22 21:37:53 +0200

    Divide_a_String_Into_Groups_of_Size_k -daily -accepted

--- src/main/java/LeetCode_Daily/UnderscoreNames.java
+++ src/main/java/LeetCode_Daily/UnderscoreNames.java
@@ -2,7 +2,7 @@ package LeetCode_Daily;
 
 public class UnderscoreNames {
     public static void main(String[] args) {
-        String name = "Divide Array Into Arrays With Max Difference";
+        String name = "Divide a String Into Groups of Size k";
         addUnderscores(name);
     }
 
