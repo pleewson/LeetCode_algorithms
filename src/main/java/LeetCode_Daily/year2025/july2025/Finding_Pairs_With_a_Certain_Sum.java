fb9d9a1cfedad62294833e0e4a95daf9f5e84ce0
pleewson <adrianplewait@gmail.com>
2025-07-06 22:37:41 +0200

    Finding_Pairs_With_a_Certain_Sum -medium -daily -accepted

---
+++ src/main/java/LeetCode_Daily/year2025/july2025/Finding_Pairs_With_a_Certain_Sum.java
@@ -0,0 +1,7 @@
+package LeetCode_Daily.year2025.july2025;
+
+import LeetCode_Daily.year2025.july2025.additional.FindSumPairs;
+// https://leetcode.com/problems/finding-pairs-with-a-certain-sum/submissions/1688846220/?envType=daily-question&envId=2025-07-06
+public class Finding_Pairs_With_a_Certain_Sum {
+    private final FindSumPairs findSumPairs = new FindSumPairs(new int[]{1, 2, 3}, new int[]{4, 5, 6});
+}
